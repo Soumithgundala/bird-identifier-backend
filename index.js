a2c9e84874de39b71e65b3cbed19a46f0f8c1796
soumithgundala <soumithgundala@gmail.com>
2025-03-24 12:45:34 +0530

    added port

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 // Configure CORS middleware
 app.use(
   cors({
